fcfd0860022f7ea7820904c101732da08727f0f0
danvillar-sketch <dan.villar@duocuc.cl>
2026-07-02 21:53:06 -0400

    prueba2xd

--- src/main/java/cl/AlParis/notificaciones/dto/RequestNotificasion.java
+++ src/main/java/cl/AlParis/notificaciones/dto/RequestNotificasion.java
@@ -2,23 +2,19 @@ package cl.AlParis.notificaciones.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-<<<<<<< HEAD
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-=======
 
->>>>>>> 3f2b061ffecc7eb19b35eb2555720ea0ffa0d155
-
-public record RequestNotificasion(  
+public record RequestNotificasion(
 
     @NotBlank(message = "El ID del pedido es obligatorio")
     String pedidoId,
 
-    @NotBlank(message = "El ID del cliente es obligatorio") 
+    @NotBlank(message = "El ID del cliente es obligatorio")
     String clienteId,
 
     @NotBlank(message = "El destinatario es obligatorio")
-    @Email(message = "El formato del correo electrónico no es válido") 
+    @Email(message = "El formato del correo electrónico no es válido")
     String destinatario,
 
     @NotBlank(message = "El tipo de canal es obligatorio")
@@ -30,4 +26,5 @@ public record RequestNotificasion(
 
     @NotBlank(message = "El mensaje es obligatorio")
     String mensaje
+
 ) {}
